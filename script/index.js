4102ff275a7dc06135a4071cbeacd9305982c58e
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-03-20 15:43:36 +0300

    feat: add event listener

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@ import i18Obj from './key.js';
 function createButton(line, from, to, keyList, specialButton = specialButtonList) {
     for (let i = from; i < to; i++) {
         let button = document.createElement('div');
-        button.className = 'keyboard_button';
+        button.className = 'keyboard__button';
 
         if (specialButton.includes(keyList[i][0].trim())) {
             button.classList.add(`button-${keyList[i][0].trim().split(' ').join('-').toLowerCase()}`);
@@ -19,7 +19,7 @@ function createButton(line, from, to, keyList, specialButton = specialButtonList
             subBut1.textContent = keyList[i][0].split(' ')[1];
 
             let subBut2 = document.createElement('div');
-            subBut2.className = 'keyboard_button-sub2';
+            subBut2.className = 'keyboard__button-sub2';
             subBut2.textContent = keyList[i][0].split(' ')[0];
 
             button.append(subBut1, subBut2);
@@ -38,24 +38,24 @@ let specialButtonList = ['backspace', 'tab', 'ENTER', 'DEL', 'Ctrl', 'space', 'S
 let keysEn = Object.entries(i18Obj.en);
 
 let lineOne = document.createElement('div');
-lineOne.className = 'keyboard_line-one';
+lineOne.className = 'keyboard__line-one';
 createButton(lineOne, 0, 14, keysEn);
 
 let lineTwo = document.createElement('div');
-lineTwo.className = 'keyboard_line-two';
+lineTwo.className = 'keyboard__line-two';
 createButton(lineTwo, 14, 29, keysEn);
 
 let lineThree = document.createElement('div');
-lineThree.className = 'keyboard_line-three';
+lineThree.className = 'keyboard__line-three';
 createButton(lineThree, 29, 42, keysEn);
 
 let lineFour = document.createElement('div');
-lineFour.className = 'keyboard_line-four';
+lineFour.className = 'keyboard__line-four';
 createButton(lineFour, 42, 55, keysEn);
 
 
 let lineFive = document.createElement('div');
-lineFive.className = 'keyboard_line-five';
+lineFive.className = 'keyboard__line-five';
 createButton(lineFive, 55, 64, keysEn);
 
 let keyboard = document.createElement('div');
@@ -64,6 +64,34 @@ keyboard.append(lineOne, lineTwo, lineThree, lineFour, lineFive);
 document.body.appendChild(keyboard);
 
 
+
+let isShiftPressed = false;
+let isAltPressed = false;
+let isCapsLock = false;
+
+
+// keyboard.addEventListener('click', clickButton)
+keyboard.addEventListener('mousedown', function (event) {
+    clickButton(event, 'down');
+})
+keyboard.addEventListener('mouseup', function (event) {
+    clickButton(event, 'up');
+})
+
+
+function clickButton(event, query) {
+
+    event.target.classList.toggle('keyboard__button_press')
+
+    // event.target.classList.add('keyboard__button_press')
+    // if(event.target.classList.contains('special-button')) {
+    //     pressSpecialButton(event)
+    // }
+}
+
+function pressSpecialButton(event) {
+
+}
 
 
 
