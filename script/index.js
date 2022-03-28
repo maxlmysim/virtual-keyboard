e48c72dfe887908c9377694f1d3c326c9d47d47d
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-03-28 21:37:43 +0300

    feat: add event listener for keyboard

--- script/index.js
+++ script/index.js
@@ -1,9 +1,10 @@
 import i18Obj from './key.js';
 
-function createButton(line, from, to, keyList, specialButton = specialButtonList) {
+function createButtons(line, from, to, keyList, specialButton = specialButtonList) {
     for (let i = from; i < to; i++) {
         let button = document.createElement('div');
         button.className = 'keyboard__button';
+        button.dataset.keyCode = keyList[i][1];
 
         if (specialButton.includes(keyList[i][0].trim())) {
             button.classList.add(`button-${keyList[i][0].trim().split(' ').join('-').toLowerCase()}`);
@@ -12,10 +13,10 @@ function createButton(line, from, to, keyList, specialButton = specialButtonList
             line.append(button);
             continue;
         }
-        button.setAttribute('key-code', `${keyList[i][1]}`);
+
         if (keyList[i][0].split(' ').length === 2) {
             let subBut1 = document.createElement('div');
-            subBut1.className = 'keyboard_button-sub1';
+            subBut1.className = 'keyboard__button-sub1';
             subBut1.textContent = keyList[i][0].split(' ')[1];
 
             let subBut2 = document.createElement('div');
@@ -39,24 +40,24 @@ let keysEn = Object.entries(i18Obj.en);
 
 let lineOne = document.createElement('div');
 lineOne.className = 'keyboard__line-one';
-createButton(lineOne, 0, 14, keysEn);
+createButtons(lineOne, 0, 14, keysEn);
 
 let lineTwo = document.createElement('div');
 lineTwo.className = 'keyboard__line-two';
-createButton(lineTwo, 14, 29, keysEn);
+createButtons(lineTwo, 14, 29, keysEn);
 
 let lineThree = document.createElement('div');
 lineThree.className = 'keyboard__line-three';
-createButton(lineThree, 29, 42, keysEn);
+createButtons(lineThree, 29, 42, keysEn);
 
 let lineFour = document.createElement('div');
 lineFour.className = 'keyboard__line-four';
-createButton(lineFour, 42, 55, keysEn);
+createButtons(lineFour, 42, 55, keysEn);
 
 
 let lineFive = document.createElement('div');
 lineFive.className = 'keyboard__line-five';
-createButton(lineFive, 55, 64, keysEn);
+createButtons(lineFive, 55, 64, keysEn);
 
 let keyboard = document.createElement('div');
 keyboard.className = 'keyboard';
@@ -64,37 +65,49 @@ keyboard.append(lineOne, lineTwo, lineThree, lineFour, lineFive);
 document.body.appendChild(keyboard);
 
 
-
 let isShiftPressed = false;
 let isAltPressed = false;
 let isCapsLock = false;
-
-
-// keyboard.addEventListener('click', clickButton)
-keyboard.addEventListener('mousedown', function (event) {
-    clickButton(event, 'down');
-})
-keyboard.addEventListener('mouseup', function (event) {
-    clickButton(event, 'up');
-})
-
+let listButtons = Array.from(document.querySelectorAll('[data-key-code]'));
 
 function clickButton(event, query) {
+    if(query === 'down') {
+        event.classList.add('keyboard__button_press');
+    }
 
-    event.target.classList.toggle('keyboard__button_press')
-
-    // event.target.classList.add('keyboard__button_press')
-    // if(event.target.classList.contains('special-button')) {
-    //     pressSpecialButton(event)
-    // }
+    if(query === 'up') {
+        event.classList.remove('keyboard__button_press');
+    }
 }
 
-function pressSpecialButton(event) {
+window.addEventListener('keydown', function (event) {
+    listButtons.find(item => {
+        if (+item.dataset.keyCode === event.keyCode) {
 
-}
+            clickButton(item, 'down')
+            return true;
+        }
+    });
+});
 
+window.addEventListener('keyup', function (event) {
+    listButtons.find(item => {
+        if (+item.dataset.keyCode === event.keyCode) {
+            clickButton(item, 'up')
+            return true;
+        }
+    });
+});
 
+keyboard.addEventListener('mousedown', function (event) {
+    if (!event.target.classList.contains('keyboard__button')) return;
+    clickButton(event.target, 'down');
+});
 
+keyboard.addEventListener('mouseup', function (event) {
+    if (!event.target.classList.contains('keyboard__button')) return;
+    clickButton(event.target, 'up');
+});
 
-
+console.log(document.querySelector('.keyboard__button').classList);
 
