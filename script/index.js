e3d9edf7a91853137352ef9fc8d0a8ae237cf8f0
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-04-25 17:33:48 +0300

    feat: add change lang

--- script/index.js
+++ script/index.js
@@ -38,40 +38,47 @@ function createButtons(line, from, to, keyList, specialButton = specialButtonLis
 
 let specialButtonList = ['backspace', 'tab', 'ENTER', 'DEL', 'Ctrl', 'space', 'Shift', 'Caps Lock', 'up arrow', 'left arrow', 'down arrow', 'right arrow', 'Win', 'Alt'];
 
-let keysEn = Object.entries(i18Obj.en);
-
-let lineOne = document.createElement('div');
-lineOne.className = 'keyboard__line-one';
-createButtons(lineOne, 0, 14, keysEn);
-
-let lineTwo = document.createElement('div');
-lineTwo.className = 'keyboard__line-two';
-createButtons(lineTwo, 14, 29, keysEn);
-
-let lineThree = document.createElement('div');
-lineThree.className = 'keyboard__line-three';
-createButtons(lineThree, 29, 42, keysEn);
-
-let lineFour = document.createElement('div');
-lineFour.className = 'keyboard__line-four';
-createButtons(lineFour, 42, 55, keysEn);
-
-
-let lineFive = document.createElement('div');
-lineFive.className = 'keyboard__line-five';
-createButtons(lineFive, 55, 64, keysEn);
-
-let keyboard = document.createElement('div');
-keyboard.className = 'keyboard';
-keyboard.append(lineOne, lineTwo, lineThree, lineFour, lineFive);
-document.body.appendChild(keyboard);
-
-
 let isShiftPressed = false;
 let isAltPressed = false;
 let isCapsLock = false;
 let currentLang = 'en';
 let listButtons = Array.from(document.querySelectorAll('[data-key-code]'));
+
+let keysEn = Object.entries(i18Obj.en);
+let keysRu = Object.entries(i18Obj.ru);
+let keyboard = document.createElement('div');
+
+function createKeyboard(lang) {
+    keyboard.innerHTML = '';
+    let lineOne = document.createElement('div');
+    lineOne.className = 'keyboard__line-one';
+    createButtons(lineOne, 0, 14, lang);
+
+    let lineTwo = document.createElement('div');
+    lineTwo.className = 'keyboard__line-two';
+    createButtons(lineTwo, 14, 29, lang);
+
+    let lineThree = document.createElement('div');
+    lineThree.className = 'keyboard__line-three';
+    createButtons(lineThree, 29, 42, lang);
+
+    let lineFour = document.createElement('div');
+    lineFour.className = 'keyboard__line-four';
+    createButtons(lineFour, 42, 55, lang);
+
+    let lineFive = document.createElement('div');
+    lineFive.className = 'keyboard__line-five';
+    createButtons(lineFive, 55, 64, lang);
+
+    keyboard.className = 'keyboard';
+    keyboard.append(lineOne, lineTwo, lineThree, lineFour, lineFive);
+    document.body.innerHTML = '';
+    document.body.appendChild(keyboard);
+    listButtons = Array.from(document.querySelectorAll('[data-key-code]'));
+}
+
+createKeyboard(keysRu);
+
 
 function clickButton(event, query) {
     if (query === 'down') {
@@ -85,8 +92,6 @@ function clickButton(event, query) {
 
 function clickButtonShift(event, query) {
     if (query === 'down') {
-        console.log(1);
-
         listButtons.forEach(item => {
             if (item.classList.contains('double-button')) {
                 item.lastChild.classList.add('hidden');
@@ -100,8 +105,6 @@ function clickButtonShift(event, query) {
     }
 
     if (query === 'up') {
-        console.log(2);
-
         listButtons.forEach(item => {
             if (item.classList.contains('double-button')) {
                 item.lastChild.classList.remove('hidden');
@@ -115,39 +118,30 @@ function clickButtonShift(event, query) {
     }
 }
 
-// function clickCapsLock() {
-//     if(isCapsLock){
-//         listButtons.forEach(item => {
-//             if (item.classList.contains('keyboard__button') &&
-//                 !item.classList.contains('double-button') &&
-//                 !item.classList.contains('special-button')) {
-//                 item.textContent = item.textContent.toLowerCase();
-//             }
-//         });
-//     } else {
-//         listButtons.forEach(item => {
-//             if (item.classList.contains('keyboard__button') &&
-//                 !item.classList.contains('double-button') &&
-//                 !item.classList.contains('special-button')) {
-//                 item.textContent = item.textContent.toUpperCase();
-//             }
-//         });
-//     }
-//
-// }
-
-window.addEventListener('keydown', function (event) {
-
-    if (event.getModifierState('Alt') && event.getModifierState('Shift')) {
-        if(currentLang === 'en') {
-            // changeLang('ru');
-        } else {
-            // changeLang('en');
-        }
+function clickButtonCapsLock(event, query) {
+    if (query === 'down') {
+        listButtons.forEach(item => {
+            if (item.classList.contains('keyboard__button') &&
+                !item.classList.contains('double-button') &&
+                !item.classList.contains('special-button')) {
+                item.textContent = item.textContent.toUpperCase();
+            }
+        });
     }
 
+    if (query === 'up') {
+        listButtons.forEach(item => {
+            if (item.classList.contains('keyboard__button') &&
+                !item.classList.contains('double-button') &&
+                !item.classList.contains('special-button')) {
+                item.textContent = item.textContent.toLowerCase();
+            }
+        });
+    }
+}
 
-    if (event.getModifierState('Shift')) clickButtonShift(event.target, 'down');
+
+window.addEventListener('keydown', function (event) {
 
     listButtons.find(item => {
         if (+item.dataset.keyCode === event.keyCode) {
@@ -156,22 +150,63 @@ window.addEventListener('keydown', function (event) {
         }
     });
 
-});
-window.addEventListener('keyup', function (event) {
+    if (event.getModifierState('Alt') && event.getModifierState('Shift')) {
+        if (currentLang === 'en') {
+            currentLang = 'ru';
+            createKeyboard(keysRu);
 
-    console.log(event);
-    if (event.getModifierState('Shift')) {
-        clickButtonShift(event.target, 'up');
-        console.log(2);
+        } else {
+            currentLang = 'en';
+            createKeyboard(keysEn);
+        }
 
+        listButtons.find(item => {
+            if (+item.dataset.keyCode === 16) {
+                clickButton(item, 'down');
+                return true;
+            }
+        });
+
+        listButtons.find(item => {
+            if (+item.dataset.keyCode === 18) {
+                clickButton(item, 'down');
+                return true;
+            }
+        });
     }
 
+    if (event.key === 'Shift') {
+        clickButtonShift(event.target, 'down');
+    }
+
+    if (event.key === 'CapsLock') {
+        isCapsLock = !isCapsLock;
+        if (isCapsLock) {
+            clickButtonCapsLock(event.target, 'down');
+        } else {
+            clickButtonCapsLock(event.target, 'up');
+        }
+    }
+
+    if (event.keyCode === 9 || event.keyCode === 18) {
+        event.preventDefault();
+    }
+
+
+});
+
+window.addEventListener('keyup', function (event) {
     listButtons.find(item => {
         if (+item.dataset.keyCode === event.keyCode) {
             clickButton(item, 'up');
             return true;
         }
     });
+
+    if (event.key === 'Shift') {
+        clickButtonShift(event.target, 'up');
+    }
+
 });
 
 keyboard.addEventListener('mousedown', function (event) {
